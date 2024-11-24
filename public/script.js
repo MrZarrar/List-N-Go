893c9cf80a1bef6a665d78f262ce04173256204f
Mushaf Zarrar <m.mushafzarrar@gmail.com>
2024-11-24 18:01:52 +0000

    removed local

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 // Determine the API URL based on the environment variable
-const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/get-price';  // Fallback to local if no env var
+const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'https://listngo-mrzarrars-projects.vercel.app/get-price';  // Fallback to local if no env var
 
 // Save the shopping list and total price to Local Storage
 function saveToLocalStorage() {
